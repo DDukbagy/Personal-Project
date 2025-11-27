dc1de6c7ca139f347121e81b1ab6f853c8be00a1
DDukbagy <86943935+DDukbagy@users.noreply.github.com>
2025-11-27 14:40:22 +0000

    module practice

--- Practice/module.py
+++
@@ -1,4 +0,0 @@
-import mod1
-add = mod1.AddCal()
-add.setdata(3,4)
-print(add.add())
--- module_Practice/mod1.py
+++ module_Practice/mod1.py
@@ -1,23 +1,23 @@
-'''
-class AddCal:
+
+class MulCal:
     def setdata(self, first, second):
         self.first = first
         self.second = second
 
-    def add(self):
-        result = self.first + self.second
+    def mul(self):
+        result = self.first * self.second
         return result
 
 if __name__ == "__main__":
     print("This file is main file!")
 else:
     print("This file is \""+__name__+"\" file!")
-'''
+
 
 """
 * __name__
 - 모듈 이름을 저장하는 변수
-- __name__가 있는 파일에서 직접 실행하면 값이 "__main__"로 저장됨
-- 다른 모듈에서 불러와서 실행하면 __name__가 그 파일의 이름으로 저장됨
+- '__name__'가 있는 파일에서 직접 실행하면 값이 '__main__'로 저장됨
+- 다른 모듈에서 불러와서 실행하면 '__name__'가 그 파일의 이름으로 저장됨
 - 직접 실행용 코드와 import용 코드를 구분할 때 사용함
 """
